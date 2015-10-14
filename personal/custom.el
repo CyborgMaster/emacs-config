3a81cddb2652f2eb1b62ce82dd1ba3f54b2e8846
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2015-10-13 20:39:21 -0600

    Replace ruby-mode with enh-ruby mode

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(enh-ruby-bounce-deep-indent t)
+ '(enh-ruby-deep-indent-paren nil)
  '(web-mode-code-indent-offset 2)
  '(web-mode-css-indent-offset 2)
  '(web-mode-markup-indent-offset 2))
