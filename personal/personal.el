6306cab7340542e7b805a760d5a3173dd96d6ad8
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2015-10-13 18:06:15 -0600

    Comment cusomizations

--- personal/personal.el
+++ personal/personal.el
@@ -1,10 +1,15 @@
+;; Use Revive+ to restore open frames and buffers.
+;;
+;; This is a very small library taken from
+;; https://github.com/martialboniou/revive-plus.  It's not on MELPA
+;; and so is installed inside of /vendor
 (prelude-require-package 'revive)
-
 (require 'revive+)
 (setq revive-plus:all-frames t)
 (revive-plus:demo)
 
+;; Turn on helm for awesome auto completion
 (require 'prelude-helm-everywhere)
 
-;; The Monaco font for OSX is built without anti aliasing
+;; The Monaco font for OSX is intended to be used without anti aliasing
 (setq mac-allow-anti-aliasing nil)
