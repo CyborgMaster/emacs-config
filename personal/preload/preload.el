3dc84cbfb7c1ca51a1309145804e2fe9e6037c1d
Jeremy Mickelson <Jeremy.Mickelson@gmail.com>
2015-10-13 18:08:13 -0600

    Commit the customize file and add more comments

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,5 +1,7 @@
+;; Use the command key as meta and the option key as super
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
 
+;; Load our custom theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (setq prelude-theme 'nova)
