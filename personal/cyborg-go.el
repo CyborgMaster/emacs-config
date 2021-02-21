663f1efeb3179dcda1077cf9420e1aa35a361efb
Jeremy Mickelson <jmickelson@tesla.com>
2021-02-21 09:33:10 -0800

    more fixes to go-test.el

--- personal/cyborg-go.el
+++ personal/cyborg-go.el
@@ -45,11 +45,17 @@
 ;; The normal gotest package has a bug with testify.  PR here:
 ;; https://github.com/nlamirault/gotest.el/pull/70.  I forked it to rebase his
 ;; PR on master.
+;;
+;; 2020-02-21: There was another bug with the "-race" flag being put in the
+;; wrong order, causing it to be ignored.  I opened a PR to fix it here:
+;; https://github.com/nlamirault/gotest.el/pull/80.
+;;
+;; I merged the two pending fixes into a combined fixes branch for use here.
 (use-package gotest
   :quelpa (gotest
            :fetcher github
            :repo "CyborgMaster/gotest.el"
-           :branch "testify-rebased")
+           :branch "combined-fixes")
   :config (setq-default go-test-args "-race"))
 
 ;; Optional - provides fancier overlays.
